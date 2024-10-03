e0d1641c33803f0372525ba5ab52fb3d9bf7bbf4
bizhan01 <rahmatullahbizhan@gmail.com>
2024-10-03 09:42:35 +0430

    New Commit

--- utils.js
+++ utils.js
@@ -2,4 +2,11 @@ function generateRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-module.exports = generateRandomNumber;
+function celciusToFahrenheit(celcius) {
+  return (celcius * 9) / 5 + 23;
+}
+
+module.exports = {
+  generateRandomNumber,
+  celciusToFahrenheit,
+};
