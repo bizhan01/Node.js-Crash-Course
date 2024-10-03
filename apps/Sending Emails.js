74d828e980a5bcf86cc33eb5103359125e621acd
bizhan01 <rahmatullahbizhan@gmail.com>
2024-10-03 15:44:46 +0430

    New Commit

---
+++ apps/Sending Emails.js
@@ -0,0 +1,9 @@
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "your_email@gmail.com",
+    pass: "your_password",
+  },
+});
