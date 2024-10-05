251dadc1b8cc900c500de7953b5fd9020d546632
bizhan01 <rahmatullahbizhan@gmail.com>
2024-10-05 09:37:54 +0430

    Json Error

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ import http from "http";
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-type", "text/html");
-  res.statusCode = 404;
+  // res.setHeader("Content-type", "text/html");
+  // res.statusCode = 404;
 
-  res.end("<h1>Hello World</h1>");
+  res.writeHead(500, { "content-type": "application/json" });
+  res.end(JSON.stringify({ message: "Server Error" }));
 });
 
 server.listen(PORT, () => {
