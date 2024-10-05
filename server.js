3ec14173b8f1d1241036b4740acdac8aa4fe40c0
bizhan01 <rahmatullahbizhan@gmail.com>
2024-10-05 11:56:02 +0430

    Simple Routing

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ import http from "http";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  // res.setHeader("Content-type", "text/html");
-  // res.statusCode = 404;
-  console.log(req.url);
-  console.log(req.method);
-
-  // res.writeHead(500, { "content-type": "application/json" });
-  // res.end(JSON.stringify({ message: "Server Error" }));
-  res.setHeader("Content-type", "text/html");
-  res.end("<h1>Hello World</h1>");
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end("<h1>Hello World</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end("<h1>About</h1>");
+      } else {
+        res.writeHead(404, { "content-type": "text/html" });
+        res.end("<h1>Not Found</h1>");
+      }
+    } else {
+      throw new Error("Method Not Allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "content-type": "text/html" });
+    res.end("<h1>Server Error</h1>");
+  }
 });
 
 server.listen(PORT, () => {
