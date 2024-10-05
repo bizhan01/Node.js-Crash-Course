ab59c9642fbd623bc5f55f1d437d7d6c272efe66
bizhan01 <rahmatullahbizhan@gmail.com>
2024-10-05 11:27:56 +0430

    Req Object

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
   // res.setHeader("Content-type", "text/html");
   // res.statusCode = 404;
+  console.log(req.url);
+  console.log(req.method);
 
-  res.writeHead(500, { "content-type": "application/json" });
-  res.end(JSON.stringify({ message: "Server Error" }));
+  // res.writeHead(500, { "content-type": "application/json" });
+  // res.end(JSON.stringify({ message: "Server Error" }));
+  res.setHeader("Content-type", "text/html");
+  res.end("<h1>Hello World</h1>");
 });
 
 server.listen(PORT, () => {
