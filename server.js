cc72189e3a51ace627ee3e9f8a93fd3010d68795
bizhan01 <rahmatullahbizhan@gmail.com>
2024-10-06 11:18:53 +0430

    File loading

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 import { log } from "console";
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+// Get Current Path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res) => {
   try {
     if (req.method === "GET") {
+      let filePath;
       if (req.url === "/") {
-        res.writeHead(200, { "content-type": "text/html" });
-        res.end("<h1>Hello World</h1>");
+        filePath = path.join(__dirname, "public", "index.html");
       } else if (req.url === "/about") {
-        res.writeHead(200, { "content-type": "text/html" });
-        res.end("<h1>About</h1>");
+        filePath = path.join(__dirname, "public", "about.html");
       } else {
-        res.writeHead(404, { "content-type": "text/html" });
-        res.end("<h1>Not Found</h1>");
+        throw new Error(" Not Found");
       }
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
     } else {
       throw new Error("Method Not Allowed");
     }
