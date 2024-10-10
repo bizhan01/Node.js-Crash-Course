18545c3c895f36e3e40ba26b7bfb462a88442de8
bizhan01 <rahmatullahbizhan@gmail.com>
2024-10-10 09:43:16 +0430

    Path Demo Final

--- pathDemo.js
+++ pathDemo.js
@@ -1,4 +1,5 @@
 import path from "path";
+import url from "url";
 
 const filePath = "./dir1/dir2/test.txt";
 
@@ -13,3 +14,14 @@ console.log(path.extname(filePath));
 
 // parse()
 console.log(path.parse(filePath));
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// join()
+const filePath2 = path.join(__dirname, "dir1", "dir2", "test.txt");
+console.log(filePath2);
+
+// resolve()
+const filePath3 = path.resolve(__dirname, "dir1", "dir2", "test.txt");
+console.log(filePath3);
